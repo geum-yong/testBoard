9a7a494516fdf3d78b7d30360b36ff2d449d864b
geum-yong <gmyong21@gmail.com>
2021-10-25 23:20:00 +0900

    Feature: 초기 세팅

--- src/test/java/board/itemservice/ItemServiceApplicationTests.java
+++ src/test/java/board/itemservice/ItemServiceApplicationTests.java
@@ -1,13 +1,29 @@
 package board.itemservice;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Connection;
 
 @SpringBootTest
 class ItemServiceApplicationTests {
 
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void connection_test() {
+        try (Connection con = sqlSessionFactory.openSession().getConnection()) {
+            System.out.println("커넥션 성공");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
